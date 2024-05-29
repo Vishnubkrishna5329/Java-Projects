d187b07349490c55309d7a7e7b7996bea4ddf6dd
Vishnu B <vishnubkrishna471@gmail.com>
2024-05-29 23:43:07 +0530

    PushtoZero Java file added

---
+++ Arrays_2/PushZerosIntuition.java
@@ -0,0 +1,27 @@
+
+public class PushZerosIntuition {
+
+    public static void main(String[] args) {
+        int[] A = new int[] { 4, 0, 3, 2, 5 };
+        int nz = 0;
+        int z = 0;
+        while (z <= A.length - 1) {
+
+            if (A[z] != 0) {
+                int temp = A[nz];
+                A[nz] = A[z];
+                A[z] = temp;
+                nz++;
+                z++;
+            } else {
+                z++;
+            }
+
+        }
+
+        for (int i : A) {
+            System.out.print(i + " ");
+        }
+    }
+
+}
